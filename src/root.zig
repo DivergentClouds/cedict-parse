53db965bd92b116e56117e81edfeeef810cba7ce
DivergentClouds <27089165+DivergentClouds@users.noreply.github.com>
2023-11-25 09:07:28 -0600

    add number to diacritic tone conversion

--- src/root.zig
+++ src/root.zig
@@ -9,7 +9,7 @@ pub const Entry = struct {
     definitions: [][]u8,
 
     /// The passed allocator must be the same one used for creating the `Entry`
-    pub fn deinit(self: *@This(), allocator: std.mem.Allocator) void {
+    pub fn deinit(self: *Entry, allocator: std.mem.Allocator) void {
         allocator.free(self.traditional);
         allocator.free(self.simplified);
         allocator.free(self.pinyin);
@@ -19,9 +19,61 @@ pub const Entry = struct {
         }
         allocator.free(self.definitions);
     }
+
+    pub fn toDiacriticForm(self: *Entry, allocator: std.mem.Allocator) !void {
+        const new_pinyin = try toDiacriticSyllables(self.pinyin, allocator);
+        allocator.free(self.pinyin);
+        self.pinyin = new_pinyin;
+
+        for (self.definitions) |*definition| {
+            var definition_list = std.ArrayList(u8).init(allocator);
+            defer definition_list.deinit();
+
+            var pinyin_position = std.mem.indexOfScalar(u8, definition.*, '[');
+            var partial_definition = definition.*;
+
+            while (pinyin_position) |position| : ({
+                pinyin_position = std.mem.indexOfScalarPos(
+                    u8,
+                    partial_definition,
+                    position + 1,
+                    '[',
+                );
+            }) {
+                try definition_list.appendSlice(partial_definition[0..position]);
+                try definition_list.append('[');
+
+                var pinyin_tokens = std.mem.tokenizeScalar(
+                    u8,
+                    partial_definition[position..],
+                    ']',
+                );
+
+                const token = pinyin_tokens.next() orelse
+                    std.debug.panic("Unfinished pinyin in definition: {s}\n", .{definition.*});
+
+                const with_diacritics = try toDiacriticSyllables(
+                    token[1..],
+                    allocator,
+                );
+                defer allocator.free(with_diacritics);
+                try definition_list.appendSlice(with_diacritics);
+
+                try definition_list.append(']');
+
+                partial_definition = @constCast(pinyin_tokens.rest());
+            }
+
+            try definition_list.appendSlice(partial_definition);
+
+            allocator.free(definition.*);
+            definition.* = try definition_list.toOwnedSlice();
+        }
+    }
 };
 
 /// Returns null if given a comment. Attempts to parse `line` otherwise.
+/// Call deinit on result when done
 pub fn parseLine(line: []const u8, allocator: std.mem.Allocator) !?Entry {
     if (line[0] == '#')
         return null;
@@ -43,13 +95,17 @@ pub fn parseLine(line: []const u8, allocator: std.mem.Allocator) !?Entry {
         return error.InvalidLine);
     errdefer allocator.free(result.simplified);
 
+    line_tokens = std.mem.tokenizeScalar(u8, line_tokens.rest(), ']');
+
     const pinyin = line_tokens.next() orelse
         return error.InvalidLine;
 
-    result.pinyin = try allocator.dupe(u8, pinyin[1 .. pinyin.len - 1]);
+    // skip '['
+    result.pinyin = try allocator.dupe(u8, pinyin[1..]);
     errdefer allocator.free(result.pinyin);
 
-    const definitions = line_tokens.rest();
+    // skip trailing space
+    const definitions = line_tokens.rest()[1..];
 
     var definitions_list = std.ArrayList([]u8).init(allocator);
     defer definitions_list.deinit();
@@ -66,52 +122,172 @@ pub fn parseLine(line: []const u8, allocator: std.mem.Allocator) !?Entry {
     return result;
 }
 
-test parseLine {
-    const allocator = std.testing.allocator;
+/// Turn a string containing multiple space-seperated pinyin syllables written
+/// with numbers to written with diacritics. Caller owns both `pinyin` and
+/// returned value.
+pub fn toDiacriticSyllables(pinyin: []const u8, allocator: std.mem.Allocator) ![]u8 {
+    var syllables = std.mem.tokenizeScalar(u8, pinyin, ' ');
 
-    var list = std.ArrayList(u8).init(allocator);
-    defer list.deinit();
+    var pinyin_list = std.ArrayList(u8).init(allocator);
 
-    const writer = list.writer();
+    while (syllables.next()) |syllable| {
+        const with_diacritics = try toDiacriticSingle(syllable, allocator);
+        defer allocator.free(with_diacritics);
 
-    const line = "麵 面 [mian4] /flour/noodles/(of food) soft (not crunchy)/(slang) (of a person) ineffectual/spineless/";
-
-    var entry = try parseLine(line, allocator) orelse
-        return error.Comment;
-    defer entry.deinit(allocator);
-
-    try writer.print(
-        \\traditional: {s}
-        \\simplified: {s}
-        \\pinyin: {s}
-        \\
-        \\definitions:
-        \\
-    ,
-        .{
-            entry.traditional,
-            entry.simplified,
-            entry.pinyin,
-        },
-    );
-    for (entry.definitions) |definition| {
-        try writer.print(
-            \\    - {s}
-            \\
-        , .{definition});
+        try pinyin_list.appendSlice(with_diacritics);
+        try pinyin_list.append(' ');
     }
 
-    try std.testing.expect(std.mem.eql(u8, list.items,
-        \\traditional: 麵
-        \\simplified: 面
-        \\pinyin: mian4
-        \\
-        \\definitions:
-        \\    - flour
-        \\    - noodles
-        \\    - (of food) soft (not crunchy)
-        \\    - (slang) (of a person) ineffectual
-        \\    - spineless
-        \\
-    ));
+    _ = pinyin_list.pop(); // get rid of extra space
+
+    return try pinyin_list.toOwnedSlice();
+}
+
+/// Turn a pinyin syllable written with numbers to be written with diacritics
+/// Caller owns both `pinyin` and returned value.
+pub fn toDiacriticSingle(pinyin: []const u8, allocator: std.mem.Allocator) ![]u8 {
+    var pinyin_list = std.ArrayList(u8).init(allocator);
+    defer pinyin_list.deinit();
+
+    const vowel_count = countVowels(pinyin);
+    var vowels_parsed: usize = 0;
+    var mark_placed: bool = false;
+
+    const tone = getTone(pinyin);
+
+    var i: usize = 0;
+
+    while (i < pinyin.len) : (i += 1) {
+        const char = pinyin[i];
+
+        if (isDigit(char))
+            continue;
+
+        if (isConsonant(char) or mark_placed) {
+            try pinyin_list.append(char);
+            continue;
+        }
+
+        if (vowel_count == 1) {
+            // can only be "u:"
+            if (i < pinyin.len - 1 and pinyin[i + 1] == ':') {
+                try pinyin_list.appendSlice(
+                    toneToDiacritic(tone, "ǖǘǚǜü", &.{ 2, 2, 2, 2, 2 }),
+                );
+            } else {
+                try pinyin_list.appendSlice(
+                    toDiacriticVowel(char, tone),
+                );
+            }
+            // not nessecary due to this being the only vowel
+            // mark_placed = true;
+        } else {
+            if (!mark_placed) {
+                if (vowels_parsed == 1) {
+                    try pinyin_list.appendSlice(
+                        toDiacriticVowel(char, tone),
+                    );
+                    mark_placed = true;
+                } else {
+                    switch (char) {
+                        'a', 'e', 'A', 'E' => {
+                            try pinyin_list.appendSlice(
+                                toDiacriticVowel(char, tone),
+                            );
+                            mark_placed = true;
+                        },
+                        'o', 'O' => {
+                            try pinyin_list.appendSlice(
+                                toDiacriticVowel(char, tone),
+                            );
+                            mark_placed = true;
+                        },
+                        'u' => {
+                            if (i < pinyin.len - 1 and pinyin[i + 1] == ':') {
+                                try pinyin_list.appendSlice("ü");
+                                i += 1;
+                            } else {
+                                try pinyin_list.append(char);
+                            }
+                        },
+                        else => {
+                            try pinyin_list.append(char);
+                        },
+                    }
+                }
+            } else {
+                try pinyin_list.append(char);
+            }
+        }
+        vowels_parsed += 1;
+    }
+
+    return try pinyin_list.toOwnedSlice();
+}
+
+fn toDiacriticVowel(vowel: u8, tone: u3) []const u8 {
+    const tone_char_lengths = [_]u3{ 2, 2, 2, 2, 1 };
+    return switch (vowel) {
+        'a' => toneToDiacritic(tone, "āáǎàa", &tone_char_lengths),
+        'e' => toneToDiacritic(tone, "ēéěèe", &tone_char_lengths),
+        'i' => toneToDiacritic(tone, "īíǐìi", &tone_char_lengths),
+        'o' => toneToDiacritic(tone, "ōóǒòo", &tone_char_lengths),
+        'u' => toneToDiacritic(tone, "ūúǔùu", &tone_char_lengths),
+        'A' => toneToDiacritic(tone, "ĀÁǍÀA", &tone_char_lengths),
+        'E' => toneToDiacritic(tone, "ĒÉĚÈE", &tone_char_lengths),
+        'I' => toneToDiacritic(tone, "ĪÍǏÌI", &tone_char_lengths),
+        'O' => toneToDiacritic(tone, "ŌÓǑÒO", &tone_char_lengths),
+        'U' => toneToDiacritic(tone, "ŪÚǓÙU", &tone_char_lengths),
+        else => std.debug.panic("Invalid Character '{c}'\n", .{vowel}),
+    };
+}
+
+fn toneToDiacritic(tone: u3, canidates: []const u8, bytes_per_canidate: *const [5]u3) []const u8 {
+    var offset: usize = 0;
+    // don't include tone canidate in offset
+    for (0..tone - 1) |i| {
+        offset += bytes_per_canidate[i];
+    }
+
+    // tone is 1 indexed, but array access is 0 indexed
+    return canidates[offset .. offset + bytes_per_canidate[tone - 1]];
+}
+
+fn isDigit(char: u8) bool {
+    return switch (char) {
+        '0'...'9' => true,
+        else => false,
+    };
+}
+
+fn isVowel(char: u8) bool {
+    const vowels = "aeiouAEIOU";
+
+    return for (vowels) |vowel| {
+        if (char == vowel)
+            break true;
+    } else false;
+}
+
+fn countVowels(string: []const u8) usize {
+    var result: usize = 0;
+    for (string) |char| {
+        if (isVowel(char))
+            result += 1;
+    }
+    return result;
+}
+
+/// Turns a digit '1' through '5' into an integer 1 through 5.
+/// Asserts that digit is in range.
+fn getTone(string: []const u8) u3 {
+    const tone_digit = string[string.len - 1];
+
+    std.debug.assert(tone_digit >= '1' and tone_digit <= '5');
+
+    return @intCast(tone_digit - '0');
+}
+
+fn isConsonant(char: u8) bool {
+    return !isVowel(char) and std.ascii.isAlphabetic(char);
 }
---
+++ src/test.zig
@@ -0,0 +1,165 @@
+const std = @import("std");
+const testing = std.testing;
+const root = @import("root.zig");
+
+test "toDiacriticSyllable" {
+    const allocator = testing.allocator;
+
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    const writer = list.writer();
+
+    const syllables: []const []const u8 = &.{
+        "ma1",
+        "ma2",
+        "ma3",
+        "ma4",
+        "ma5",
+        "huan1",
+        "huan2",
+        "huan3",
+        "huan4",
+        "huan5",
+        "zhuai1",
+        "zhuai2",
+        "zhuai3",
+        "zhuai4",
+        "zhuai5",
+        "zui1",
+        "zui2",
+        "zui3",
+        "zui4",
+        "zui5",
+        "jiu1",
+        "jiu2",
+        "jiu3",
+        "jiu4",
+        "jiu5",
+        "Ou1",
+        "Ou2",
+        "Ou3",
+        "Ou4",
+        "Ou5",
+        "lu:e1",
+        "lu:e2",
+        "lu:e3",
+        "lu:e4",
+        "lu:e5",
+        "xx5",
+    };
+
+    for (syllables) |syllable| {
+        const with_diacritics = try root.toDiacriticSingle(syllable, allocator);
+        defer allocator.free(with_diacritics);
+        try writer.print("{s} -> {s}\n", .{
+            syllable,
+            with_diacritics,
+        });
+    }
+
+    try testing.expectEqualSlices(u8, list.items,
+        \\ma1 -> mā
+        \\ma2 -> má
+        \\ma3 -> mǎ
+        \\ma4 -> mà
+        \\ma5 -> ma
+        \\huan1 -> huān
+        \\huan2 -> huán
+        \\huan3 -> huǎn
+        \\huan4 -> huàn
+        \\huan5 -> huan
+        \\zhuai1 -> zhuāi
+        \\zhuai2 -> zhuái
+        \\zhuai3 -> zhuǎi
+        \\zhuai4 -> zhuài
+        \\zhuai5 -> zhuai
+        \\zui1 -> zuī
+        \\zui2 -> zuí
+        \\zui3 -> zuǐ
+        \\zui4 -> zuì
+        \\zui5 -> zui
+        \\jiu1 -> jiū
+        \\jiu2 -> jiú
+        \\jiu3 -> jiǔ
+        \\jiu4 -> jiù
+        \\jiu5 -> jiu
+        \\Ou1 -> Ōu
+        \\Ou2 -> Óu
+        \\Ou3 -> Ǒu
+        \\Ou4 -> Òu
+        \\Ou5 -> Ou
+        \\lu:e1 -> lüē
+        \\lu:e2 -> lüé
+        \\lu:e3 -> lüě
+        \\lu:e4 -> lüè
+        \\lu:e5 -> lüe
+        \\xx5 -> xx
+        \\
+    );
+}
+
+test "parseLine" {
+    const allocator = testing.allocator;
+
+    var list = std.ArrayList(u8).init(allocator);
+    defer list.deinit();
+
+    const writer = list.writer();
+
+    const line = "麵 面 [mian4] /flour/noodles/(of food) soft (not crunchy)/(slang) (of a person) ineffectual/spineless/";
+
+    var entry = try root.parseLine(line, allocator) orelse
+        return error.UnexpectedComment;
+    defer entry.deinit(allocator);
+
+    try writer.print(
+        \\traditional: {s}
+        \\simplified: {s}
+        \\pinyin: {s}
+        \\
+        \\definitions:
+        \\
+    , .{
+        entry.traditional,
+        entry.simplified,
+        entry.pinyin,
+    });
+    for (entry.definitions) |definition| {
+        try writer.print(
+            \\    - {s}
+            \\
+        , .{definition});
+    }
+
+    try testing.expectEqualSlices(u8, list.items,
+        \\traditional: 麵
+        \\simplified: 面
+        \\pinyin: mian4
+        \\
+        \\definitions:
+        \\    - flour
+        \\    - noodles
+        \\    - (of food) soft (not crunchy)
+        \\    - (slang) (of a person) ineffectual
+        \\    - spineless
+        \\
+    );
+}
+
+test "Entry.toDiacriticForm" {
+    const allocator = testing.allocator;
+
+    const line = "主意 主意 [zhu3 yi5] /plan/idea/decision/CL:個|个[ge4]/Beijing pr. [zhu2 yi5]/";
+
+    var entry = try root.parseLine(line, allocator) orelse
+        return error.UnexpectedComment;
+    defer entry.deinit(allocator);
+
+    try entry.toDiacriticForm(allocator);
+
+    try testing.expectEqualSlices(u8, entry.pinyin, "zhǔ yi");
+
+    try testing.expectEqualSlices(u8, entry.definitions[3], "CL:個|个[gè]");
+    try testing.expectEqualSlices(u8, entry.definitions[4], "Beijing pr. [zhú yi]");
+}
